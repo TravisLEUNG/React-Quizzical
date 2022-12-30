c6ce2adf6aefa82e6c6a29ac31218298499c3270
TravisLEUNG <mhl.travis@gmail.com>
2022-12-30 03:05:27 -0500

    add selections for setting

---
+++ src/components/Selection.tsx
@@ -0,0 +1,35 @@
+// Required props
+interface RequiredProps {
+	label: string;
+	options: Array<{ value: string; label: string }>;
+	onSelected: (newValue: string) => void;
+}
+
+export default function Selection({
+	label,
+	options,
+	onSelected,
+}: RequiredProps) {
+	const handleSelection = (event: React.FormEvent<HTMLInputElement>) => {
+		onSelected(event.currentTarget.value! ?? options[0].value);
+	};
+
+	const optionList = options.map((option) => (
+		<option key={option.value} value={option.value}>
+			{option.label}
+		</option>
+	));
+	return (
+		<div className='flex gap-4'>
+			<label className='text-md flex w-auto text-center align-middle font-bold leading-8 text-gray-800'>
+				{label}
+			</label>
+			<select
+				onChange={() => handleSelection}
+				className='text-l text-l w-full appearance-none rounded-lg border-blue-500 bg-gray-200 px-2 text-center text-sm text-gray-600 outline-none  outline-offset-0 hover:outline-2 hover:outline-blue-500'
+			>
+				{optionList}
+			</select>
+		</div>
+	);
+}
--- src/views/StartingPage.tsx
+++ src/views/StartingPage.tsx
@@ -1,3 +1,5 @@
+import Selection from "../components/Selection";
+
 // Required props
 interface RequiredProps {
 	handleStarting: () => void;
@@ -10,6 +12,75 @@ export default function StartingPage({ handleStarting }: RequiredProps) {
 			<p className='my-4 text-gray-600 dark:text-gray-300'>
 				Welcome to the Quizzical! If you are confident, try it.
 			</p>
+			<div className='mt-4 mb-8'>
+				<Selection
+					label='Category'
+					options={[
+						{ label: "Any Category", value: "any" },
+						{ label: "General Knowledge", value: "9" },
+						{ label: "Entertainment: Books", value: "10" },
+						{ label: "Entertainment: Film", value: "11" },
+						{ label: "Entertainment: Music", value: "12" },
+						{
+							label: "Entertainment: Musicals & Theatres",
+							value: "13",
+						},
+						{ label: "Entertainment: Television", value: "14" },
+						{ label: "Entertainment: Video Games", value: "15" },
+						{ label: "Entertainment: Board Games", value: "16" },
+						{ label: "Science & Nature", value: "17" },
+						{ label: "Science: Computers", value: "18" },
+						{ label: "Science: Mathematics", value: "19" },
+						{ label: "Mythology", value: "20" },
+						{ label: "Sports", value: "21" },
+						{ label: "Geography", value: "22" },
+						{ label: "History", value: "23" },
+						{ label: "Politics", value: "24" },
+						{ label: "Art", value: "25" },
+						{ label: "Celebrities", value: "26" },
+						{ label: "Animals", value: "27" },
+						{ label: "Vehicles", value: "28" },
+						{ label: "Entertainment: Comics", value: "29" },
+						{ label: "Science: Gadgets", value: "30" },
+						{
+							label: "Entertainment: Japanese Anime & Manga",
+							value: "31",
+						},
+						{
+							label: "Entertainment: Cartoon & Animations",
+							value: "32",
+						},
+					]}
+					onSelected={() => {
+						console.log("Hi");
+					}}
+				/>
+			</div>
+			<div className='mt-4 mb-8 flex justify-between gap-8'>
+				<Selection
+					label='Difficulty'
+					options={[
+						{ label: "Any Difficulty", value: "any" },
+						{ label: "Easy", value: "easy" },
+						{ label: "Medium", value: "medium" },
+						{ label: "Hard", value: "hard" },
+					]}
+					onSelected={() => {
+						console.log("Hi");
+					}}
+				/>
+				<Selection
+					label='Type'
+					options={[
+						{ label: "Any Type", value: "any" },
+						{ label: "Multiple Choice", value: "multiple" },
+						{ label: "True / False", value: "boolean" },
+					]}
+					onSelected={() => {
+						console.log("Hi");
+					}}
+				/>
+			</div>
 			<button
 				className='rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-300'
 				onClick={handleStarting}
